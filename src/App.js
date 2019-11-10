264feb717c0eb63a6c30b5e0cf8def4b4440de0f
bharath <bharathts07@gmail.com>
2019-11-10 13:25:51 +0900

    Add header component with associated styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         {/* Using Switch avoids accidentally rendering multiple paths at the same endpoint that may be mathced by muliple routes */}
         <Route exact={true} path="/" component={HomePage} />
