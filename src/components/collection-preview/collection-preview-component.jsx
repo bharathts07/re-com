b415b3b8f641ef575434ac27cc3c106f5eb470c0
bharath <bharathts07@gmail.com>
2019-11-10 12:23:39 +0900

    Set CollectionPreview key correctly in shop.component

--- src/components/collection-preview/collection-preview-component.jsx
+++ src/components/collection-preview/collection-preview-component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./preview-collection-component.scss";
+import "./collection-preview-component.scss";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items.map(item => (
-        <div key={item.id}>{item}</div>
+        <div key={item.id}>{item.name}</div>
       ))}
     </div>
   </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import SHOP_DATA from "./shop.data";
 
-import CollectionPreview from "./../../components/collection-preview/preview-collection-component";
+import CollectionPreview from "../../components/collection-preview/collection-preview-component";
 
 class ShopPage extends React.Component {
   constructor(props) {
@@ -18,7 +18,7 @@ class ShopPage extends React.Component {
     return (
       <div className="shop-page">
         {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview id={id} {...otherCollectionProps} />
+          <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
       </div>
     );
