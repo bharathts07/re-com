7f339ee380a1565695952d04131bc8d8a38147f2
bharath <bharathts07@gmail.com>
2020-03-01 12:23:51 +0900

    change(redux): load images from url

--- src/redux/directory/directory.data.jsx
+++ src/redux/directory/directory.data.jsx
@@ -1,32 +1,32 @@
 const SECTION_DATA = [
   {
     title: "hats",
-    imageUrl: "images/hats.png", // local file in public/images/
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     id: 1,
     linkUrl: "shop/hats"
   },
   {
     title: "jackets",
-    imageUrl: "images/jackets.png", // local file in public/images/
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     id: 2,
     linkUrl: "shop/jackets"
   },
   {
     title: "sneakers",
-    imageUrl: "images/sneakers.png", // local file in public/images/
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     id: 3,
     linkUrl: "shop/sneakers"
   },
   {
     title: "womens",
-    imageUrl: "images/womens.png", // local file in public/images/
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     size: "large",
     id: 4,
     linkUrl: "shop/womens"
   },
   {
     title: "mens",
-    imageUrl: "images/men.png", // local file in public/images/
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     size: "large",
     id: 5,
     linkUrl: "shop/mens"
